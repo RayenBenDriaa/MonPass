32c36430151eb4d5c01d9e08c18ef7024960e723
Rayen Ben Driaa <rayenbd63s@gmail.com>
2021-11-16 12:04:35 +0100

    Symfony User Crud

---
+++ serviceSymfony/src/Entity/Cin.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CinRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ORM\Entity(repositoryClass=CinRepository::class)
+ */
+class Cin
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * @Groups("post:read")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Groups("post:read")
+     */
+    private $idUser;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups("post:read")
+     */
+    private $urlImage;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups("post:read")
+     */
+    private $etat;
+
+    /**
+     * @ORM\Column(type="date")
+     * @Groups("post:read")
+     */
+    private $date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdUser(): ?int
+    {
+        return $this->idUser;
+    }
+
+    public function setIdUser(int $idUser): self
+    {
+        $this->idUser = $idUser;
+
+        return $this;
+    }
+
+    public function getUrlImage(): ?string
+    {
+        return $this->urlImage;
+    }
+
+    public function setUrlImage(string $urlImage): self
+    {
+        $this->urlImage = $urlImage;
+
+        return $this;
+    }
+
+    public function getEtat(): ?string
+    {
+        return $this->etat;
+    }
+
+    public function setEtat(string $etat): self
+    {
+        $this->etat = $etat;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
